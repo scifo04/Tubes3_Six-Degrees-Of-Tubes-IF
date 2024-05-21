a42aaf6756f153fd05455643e73a66fa50d55e1f
Haikalin <haikalassyauqi70@gmail.com>
2024-05-21 20:00:10 +0700

    feat: KMP

--- src/WpfApp1/WpfApp1/obj/Debug/net8.0-windows/WpfApp1.AssemblyInfo.cs
+++ src/WpfApp1/WpfApp1/obj/Debug/net8.0-windows/WpfApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+073207ade4753edf174fa6692d92793844f71f84")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93b82347993d3a83dd7f2bcca5f04a2f3eb3a104")]
 [assembly: System.Reflection.AssemblyProductAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
