5a620737d108f00da5a60a14ef256613c9e94b0e
Haikalin <haikalassyauqi70@gmail.com>
2024-05-23 12:17:33 +0700

    add

--- src/WpfApp1/WpfApp1/obj/Release/net8.0-windows/WpfApp1.AssemblyInfo.cs
+++ src/WpfApp1/WpfApp1/obj/Release/net8.0-windows/WpfApp1.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("WpfApp1")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93b82347993d3a83dd7f2bcca5f04a2f3eb3a104")]
 [assembly: System.Reflection.AssemblyProductAttribute("WpfApp1")]
