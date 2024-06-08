1cab6f193fa2dd9775339c1693a4e7a76313f09c
Daniel Manurung <danielmuliaputraolo@gmail.com>
2024-06-09 03:23:04 +0800

    loading screen

--- src/WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ src/WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -60,153 +60,146 @@ namespace WpfApp1
             MessageBox.Show($"Selected algorithm: {backendState.getAlgo()}");
         }
 
-        private void searchGo(object sender, RoutedEventArgs e)
+        private async void searchGo(object sender, RoutedEventArgs e)
         {
             if (backendState.getPic() != null)
             {
                 string imagePath = backendState.getPic();
-
+        
                 try
                 {
-                    // Process the image
-                    string binaryRepresentation = FingerPrintConverter.ProcessImage(imagePath);
-                    Console.WriteLine($"Binary representation: {binaryRepresentation}");
-
-                    string midDigs = FingerPrintConverter.GetMiddleDigits(binaryRepresentation, 128);
-
-                    string asciiRepresentation = FingerPrintConverter.BinaryToAscii(midDigs);
-                    Console.WriteLine($"ASCII representation: {asciiRepresentation}");
-
-                    if (!string.IsNullOrEmpty(asciiRepresentation))
+                    // Show the loading overlay
+                    loadingOverlay.Visibility = Visibility.Visible;
+        
+                    await Task.Run(() =>
                     {
-                        // Reset the dictionary before starting the search
-                        bestMatchesDict.Clear();
-
-                        // Fetch database data
-                        List<(int Id, string TempBC, string AsciiRepresentation, string Nama)> databaseData = FetchDatabaseData();
-                        List<(string PK, string Nama, string TempatLahir, string JenisKelamin, string GolonganDarah, string Alamat, string StatusPerkawinan, string Pekerjaan, string Kewarganegaraan)> BiodataData = FetchBiodata();
-
-                        // Perform search for the current ASCII representation
-                        IStringSearchAlgorithm algorithm = GetSearchAlgorithm(backendState.getAlgo());
-                        if (algorithm != null)
+                        // Process the image
+                        string binaryRepresentation = FingerPrintConverter.ProcessImage(imagePath);
+                        Console.WriteLine($"Binary representation: {binaryRepresentation}");
+        
+                        string midDigs = FingerPrintConverter.GetMiddleDigits(binaryRepresentation, 128);
+        
+                        string asciiRepresentation = FingerPrintConverter.BinaryToAscii(midDigs);
+                        Console.WriteLine($"ASCII representation: {asciiRepresentation}");
+        
+                        if (!string.IsNullOrEmpty(asciiRepresentation))
                         {
-                            // List<(int Id, int Position, int HammingDistance, double ClosenessPercentage, string Nama)> bestMatches = new List<(int, int, int, double, string)>();
-                            List<(int Id, int Position, int HammingDistance, double ClosenessPercentage, string Nama, string TempBC)> bestMatches = new List<(int, int, int, double, string, string)>();
-
-
-                            Stopwatch stopwatch = Stopwatch.StartNew();
-
-                            foreach (var data in databaseData)
+                            // Reset the dictionary before starting the search
+                            bestMatchesDict.Clear();
+        
+                            // Fetch database data
+                            List<(int Id, string TempBC, string AsciiRepresentation, string Nama)> databaseData = FetchDatabaseData();
+                            List<(string PK, string Nama, string TempatLahir, string JenisKelamin, string GolonganDarah, string Alamat, string StatusPerkawinan, string Pekerjaan, string Kewarganegaraan)> BiodataData = FetchBiodata();
+        
+                            // Perform search for the current ASCII representation
+                            IStringSearchAlgorithm algorithm = GetSearchAlgorithm(backendState.getAlgo());
+                            if (algorithm != null)
                             {
-                                var matches = algorithm.Search(data.AsciiRepresentation, asciiRepresentation).First();
-                                bestMatches.Add((data.Id, matches.Position, matches.HammingDistance, matches.ClosenessPercentage, data.Nama, data.TempBC));
-                            }
-
-
-                            stopwatch.Stop();
-
-                            if (bestMatches.Count > 0)
-                            {
-                                int nnn = bestMatches.Count;
-                                MessageBox.Show($"{nnn}");
-
-                                // Get the best match for the current ASCII representation
-                                var bestMatch = bestMatches.OrderBy(m => m.HammingDistance).First();
-
-                                List<string> matchedNames = new List<string>();
-                                foreach (var item in BiodataData)
+                                List<(int Id, int Position, int HammingDistance, double ClosenessPercentage, string Nama, string TempBC)> bestMatches = new List<(int, int, int, double, string, string)>();
+        
+                                Stopwatch stopwatch = Stopwatch.StartNew();
+        
+                                foreach (var data in databaseData)
                                 {
-                                    if (alaiRegex.TestString2(bestMatch.Nama, item.Nama))
-                                    { 
-                                        matchedNames.Add(item.Nama);
-                                        res.setName(bestMatch.Nama);
-                                        res.setNik(item.PK);
-                                        res.setBirthLoc(item.TempatLahir);
-                                        res.setGender(item.JenisKelamin);
-                                        res.setBloodType(item.GolonganDarah);
-                                        res.setAddress(item.Alamat);
-                                        res.setMarriageStatus(item.StatusPerkawinan);
-                                        res.setJob(item.Pekerjaan);
-                                        res.setCitizenship(item.Kewarganegaraan);
-                                        res.setExecTime(stopwatch.ElapsedMilliseconds);
-                                        res.setMatchPercentage(bestMatch.ClosenessPercentage);
+                                    var matches = algorithm.Search(data.AsciiRepresentation, asciiRepresentation).First();
+                                    bestMatches.Add((data.Id, matches.Position, matches.HammingDistance, matches.ClosenessPercentage, data.Nama, data.TempBC));
+                                }
+        
+                                stopwatch.Stop();
+        
+                                if (bestMatches.Count > 0)
+                                {
+                                    var bestMatch = bestMatches.OrderBy(m => m.HammingDistance).First();
+                                    List<string> matchedNames = new List<string>();
+        
+                                    foreach (var item in BiodataData)
+                                    {
+                                        if (alaiRegex.TestString2(bestMatch.Nama, item.Nama))
+                                        {
+                                            matchedNames.Add(item.Nama);
+                                            res.setName(bestMatch.Nama);
+                                            res.setNik(item.PK);
+                                            res.setBirthLoc(item.TempatLahir);
+                                            res.setGender(item.JenisKelamin);
+                                            res.setBloodType(item.GolonganDarah);
+                                            res.setAddress(item.Alamat);
+                                            res.setMarriageStatus(item.StatusPerkawinan);
+                                            res.setJob(item.Pekerjaan);
+                                            res.setCitizenship(item.Kewarganegaraan);
+                                            res.setExecTime(stopwatch.ElapsedMilliseconds);
+                                            res.setMatchPercentage(bestMatch.ClosenessPercentage);
+                                        }
                                     }
-                                }                                
-
-                                // MessageBox.Show($"Final best match found at position {bestMatch.Position} with closeness {bestMatch.ClosenessPercentage}% at id {bestMatch.Id} with nama {bestMatch.Nama}", "Final Best Match Found");
-                                MessageBox.Show($"Final best match found at position {bestMatch.Position} with closeness {bestMatch.ClosenessPercentage}% at id {bestMatch.Id} with nama {bestMatch.Nama} and Image {bestMatch.TempBC}", "Final Best Match Found");
-
-                                if (matchedNames.Count > 0)
-                                {
-                                    MessageBox.Show($"First matched name: {matchedNames[0]}", "Matched Name", MessageBoxButton.OK, MessageBoxImage.Information);
+        
+                                    Application.Current.Dispatcher.Invoke(() =>
+                                    {
+                                        MessageBox.Show($"Final best match found at position {bestMatch.Position} with closeness {bestMatch.ClosenessPercentage}% at id {bestMatch.Id} with nama {bestMatch.Nama} and Image {bestMatch.TempBC}", "Final Best Match Found");
+        
+                                        string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+                                        int srcIndex = baseDirectory.LastIndexOf("src");
+        
+                                        if (srcIndex != -1)
+                                        {
+                                            string rootPath = baseDirectory.Substring(0, srcIndex);
+                                            string relativePath = Path.Combine(rootPath, "test", bestMatch.TempBC);
+                                            string combinedPath = Path.GetFullPath(relativePath);
+        
+                                            if (File.Exists(combinedPath))
+                                            {
+                                                BitmapImage matchedImg = new BitmapImage();
+                                                matchedImg.BeginInit();
+                                                matchedImg.UriSource = new Uri(combinedPath, UriKind.Absolute);
+                                                matchedImg.EndInit();
+                                                selectedImageGay.Source = matchedImg;
+                                                TimeRun.Text = res.getExecTime().ToString() + " ms";
+                                                Percentage.Text = res.getMatchPercentage().ToString() + " %";
+                                            }
+                                            else
+                                            {
+                                                MessageBox.Show("Matched image file not found.", "File Not Found", MessageBoxButton.OK, MessageBoxImage.Warning);
+                                            }
+                                        }
+                                        else
+                                        {
+                                            MessageBox.Show("The path does not contain 'src'.", "Path Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                                        }
+        
+                                        MessageBox.Show($"Time taken: {stopwatch.ElapsedMilliseconds} milliseconds");
+                                    });
                                 }
                                 else
                                 {
-                                    MessageBox.Show("No matches found in biodata.", "No Match", MessageBoxButton.OK, MessageBoxImage.Information);
-                                }
-
-                                // Assuming bestMatch.TempBC contains the name of the image file
-                                string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
-
-                                // Find the last occurrence of "src" in the base directory path
-                                int srcIndex = baseDirectory.LastIndexOf("src");
-
-                                if (srcIndex != -1)
-                                {
-                                    // Cut the path before the last "src" occurrence
-                                    string rootPath = baseDirectory.Substring(0, srcIndex);
-
-                                    // Concatenate with "test" and the filename
-                                    string relativePath = Path.Combine(rootPath, "test", bestMatch.TempBC);
-
-                                    // Get the absolute path
-                                    string combinedPath = Path.GetFullPath(relativePath);
-
-                                    // Ensure that the combined path is pointing correctly to the intended directory
-                                    MessageBox.Show(bestMatch.TempBC);
-                                    MessageBox.Show(combinedPath);
-
-                                    if (File.Exists(combinedPath))
+                                    Application.Current.Dispatcher.Invoke(() =>
                                     {
-                                        BitmapImage matchedImg = new BitmapImage();
-                                        matchedImg.BeginInit();
-                                        matchedImg.UriSource = new Uri(combinedPath, UriKind.Absolute);
-                                        matchedImg.EndInit();
-                                        selectedImageGay.Source = matchedImg;
-                                        TimeRun.Text = res.getExecTime().ToString()+" ms";
-                                        Percentage.Text = res.getMatchPercentage().ToString()+ " %";
-                                    }
-                                    else
-                                    {
-                                        MessageBox.Show("Matched image file not found.", "File Not Found", MessageBoxButton.OK, MessageBoxImage.Warning);
-                                    }
+                                        MessageBox.Show("No matches found for this image.", "No Match", MessageBoxButton.OK, MessageBoxImage.Information);
+                                    });
                                 }
-                                else
-                                {
-                                    MessageBox.Show("The path does not contain 'src'.", "Path Error", MessageBoxButton.OK, MessageBoxImage.Error);
-    }
-
-
-                                MessageBox.Show($"Time taken: {stopwatch.ElapsedMilliseconds} milliseconds");
                             }
                             else
                             {
-                                MessageBox.Show("No matches found for this image.", "No Match", MessageBoxButton.OK, MessageBoxImage.Information);
+                                Application.Current.Dispatcher.Invoke(() =>
+                                {
+                                    MessageBox.Show("Please select an algorithm.", "Algorithm Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                                });
                             }
                         }
                         else
                         {
-                            MessageBox.Show("Please select an algorithm.", "Algorithm Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                            Application.Current.Dispatcher.Invoke(() =>
+                            {
+                                MessageBox.Show("Failed to convert to ASCII.", "Conversion Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                            });
                         }
-                    }
-                    else
-                    {
-                        MessageBox.Show("Failed to convert to ASCII.", "Conversion Error", MessageBoxButton.OK, MessageBoxImage.Error);
-                    }
+                    });
                 }
                 catch (Exception ex)
                 {
                     MessageBox.Show($"Error processing image: {ex.Message}", "Processing Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                finally
+                {
+                    // Hide the loading overlay
+                    loadingOverlay.Visibility = Visibility.Collapsed;
                 }
             }
             else
