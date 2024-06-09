3a0a1f99cfe67584e171194be017a4476519a370
Daniel Manurung <danielmuliaputraolo@gmail.com>
2024-06-09 22:55:28 +0800

    final (hopefully)

--- src/WpfApp1/obj/Debug/net8.0-windows7.0/WpfApp1.AssemblyInfo.cs
+++ src/WpfApp1/obj/Debug/net8.0-windows7.0/WpfApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9877475f90f5448eecc3e83117614172419f81f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcc607c3826c743b70060f6bbecfc11087a93aec")]
 [assembly: System.Reflection.AssemblyProductAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
